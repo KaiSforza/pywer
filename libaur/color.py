1659128bce7c1c54fbde5d4dcdb1a8d5fad45287
William Giokas <1007380@gmail.com>
2013-09-05 06:41:21 -0500

    printer: Add color and --color option
    Currently it is just a boolean with no 'when' option or 'auto' stuff.

---
+++ libaur/color.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+'''
+Color definitions for use in printing
+'''
+
+class Color():
+    def __init__(self, use_color):
+        if use_color:
+            self.black = "\x1b[30m"
+            self.red = "\x1b[31m"
+            self.green = "\x1b[32m"
+            self.yellow = "\x1b[33m"
+            self.blue = "\x1b[34m"
+            self.magenta = "\x1b[35m"
+            self.cyan = "\x1b[36m"
+            self.white = "\x1b[37m"
+
+            self.bold = "\x1b[1m"
+            self.bold_black = "\x1b[1;30m"
+            self.bold_red = "\x1b[1;31m"
+            self.bold_green = "\x1b[1;32m"
+            self.bold_yellow = "\x1b[1;33m"
+            self.bold_blue = "\x1b[1;34m"
+            self.bold_magenta = "\x1b[1;35m"
+            self.bold_cyan = "\x1b[1;36m"
+            self.bold_white = "\x1b[1;37m"
+
+        else:
+            self.black = ""
+            self.red = ""
+            self.green = ""
+            self.yellow = ""
+            self.blue = ""
+            self.magenta = ""
+            self.cyan = ""
+            self.white = ""
+
+            self.bold = ""
+            self.bold_black = ""
+            self.bold_red = ""
+            self.bold_green = ""
+            self.bold_yellow = ""
+            self.bold_blue = ""
+            self.bold_magenta = ""
+            self.bold_cyan = ""
+            self.bold_white = ""
+
+    reset = "\x1b[0m"
--- libaur/printer.py
+++ libaur/printer.py
@@ -10,6 +10,7 @@ from subprocess import call,DEVNULL
 import time
 from .errors import *
 from .__init__ import __version__
+from .color import Color
 import re
 
 CATEGORIES = {
@@ -35,7 +36,7 @@ CATEGORIES = {
         }
 
 def pretty_print_search(package, stype='search', baseurl=None, ood=True,
-        be_verbose=True):
+        be_verbose=True, color=False):
     '''
     Print out search results
 
@@ -45,7 +46,9 @@ def pretty_print_search(package, stype='search', baseurl=None, ood=True,
     baseurl (str) -- Where the AUR you are using is located
     ood (bool) -- Whether to show out of date items
     be_verbose (bool) -- Be verbose
+    color (bool) -- Whether to use color
     '''
+    _color = Color(color)
     json_output = SearchPkg(package, baseurl=baseurl,
             req_type=stype).get_results()
     for i in range(len(json_output)):
@@ -60,14 +63,29 @@ def pretty_print_search(package, stype='search', baseurl=None, ood=True,
         version = json_output[i]['Version']
         numvotes = json_output[i]['NumVotes']
         description = json_output[i]['Description']
+        is_ood = json_output[i]['OutOfDate']
 
         if be_verbose:
-            print('aur/{} {} {}({})\n    {}'.format(name, version, is_ood,
-                numvotes, description))
+            if json_output[i]['OutOfDate'] == 0:
+                print('{4}aur/{7}{5}{0} {6}{1}{7} ({2})\n    {3}'.format(
+                    name, version, numvotes, description,
+                    _color.bold_magenta, _color.bold, _color.bold_green,
+                    _color.reset))
+            else:
+                if not ood:
+                    continue
+                if color:
+                    print('{4}aur/{7}{5}{0} {6}{1}{7} ({2})\n    {3}'.format(
+                        name, version, numvotes, description,
+                        _color.bold_magenta, _color.bold, _color.bold_red,
+                        _color.reset))
+                else:
+                    print('aur/{} {} <!> ({})\n    {}'.format(
+                        name, version, numvotes, description))
         else:
             print(name)
 
-def pretty_print_simple_info(packages, baseurl=None, ood=True):
+def pretty_print_simple_info(packages, baseurl=None, ood=True, color=False):
     '''
     Get some simple info from an AUR
 
@@ -75,7 +93,9 @@ def pretty_print_simple_info(packages, baseurl=None, ood=True):
     package (str) -- A string to search for
     baseurl (str) -- Where the AUR you are using is located
     ood (bool) -- Whether to show out of date items
+    color (bool) -- Whether to use color
     '''
+    _color = Color(color)
     json_output = InfoPkg(packages,
             baseurl=baseurl).get_results()
     localized = time.localtime()
@@ -88,14 +108,16 @@ def pretty_print_simple_info(packages, baseurl=None, ood=True):
         info_dict = {}
         # Out of date or not
         if json_output[i]['OutOfDate'] == 0:
-            info_dict['Out Of Date'] = '{:<15}: {}'.format('Out Of Date', 'No')
+            info_dict['Out Of Date'] = '{:<15}: {}{}{}'.format('Out Of Date',
+                    _color.bold_green, 'No', _color.reset)
         else:
             if not ood:
                 continue
             oodtime = time.ctime(json_output[i]['OutOfDate'] + time.timezone
                     - tzdiff)
-            info_dict['Out Of Date'] = '{:<15}: {} (since {})'\
-                    .format('Out Of Date', 'Yes', oodtime)
+            info_dict['Out Of Date'] = '{:<15}: {}{}{} (since {})'\
+                    .format('Out Of Date', 
+                    _color.bold_red, 'Yes', _color.reset, oodtime)
         # Format the date fields using 'time'
         for field in ['FirstSubmitted', 'LastModified']:
             if field == 'FirstSubmitted':
@@ -105,24 +127,31 @@ def pretty_print_simple_info(packages, baseurl=None, ood=True):
             sec_time = json_output[i][field]
             info_dict[pretty_field] = '{:<15}: {}'.format(pretty_field,
                     time.ctime(json_output[i][field] + time.timezone - tzdiff))
-        info_dict['repo'] = '{:<15}: {}'.format('Repository', 'aur')
+        info_dict['repo'] = '{:<15}: {}{}{}'.format(
+                'Repository', _color.bold_magenta, 'aur', _color.reset)
         # If it's installed, add the [installed] flag to the name
         if call(['/usr/bin/pacman', '-Qq', json_output[i]['Name']],
                 stdout=DEVNULL, stderr=DEVNULL) == 0:
-            installed = ' [installed]'
+            installed = ' {}[{}installed{}]{}'.format(
+                    _color.bold_blue, _color.bold_green, _color.bold_blue,
+                    _color.reset)
         else:
             installed = ''
-        info_dict['Name'] = '{:<15}: {}{}'.format('Name',
-                json_output[i]['Name'], installed)
+        info_dict['Name'] = '{:<15}: {}{}{}{}'.format('Name', _color.bold,
+                json_output[i]['Name'], _color.reset, installed)
         # Get the easy, plain strings
-        for field in ['Version', 'URL', 'License', 'Maintainer',
+        info_dict['Version'] = '{:<15}: {}{}{}'.format('Version',
+                _color.bold_green, json_output[i]['Version'], _color.reset)
+        for field in ['URL', 'License', 'Maintainer',
                 'Description']:
             info_dict[field] = '{:<15}: {}'.format(field,
                     json_output[i][field])
         info_dict['Votes'] = '{:<15}: {}'.format(field,
                 json_output[i]['NumVotes'])
-        info_dict['AUR Page'] = '{:<15}: {}/packages/{}'.format('AUR Page',
-                baseurl, json_output[i]['Name'])
+        info_dict['URL'] = '{:<15}: {}{}{}'.format('Version',
+                _color.bold_blue, json_output[i]['URL'], _color.reset)
+        info_dict['AUR Page'] = '{:<15}: {}{}/packages/{}{}'.format('AUR Page',
+                _color.bold_blue, baseurl, json_output[i]['Name'], _color.reset)
         info_dict['Category'] = '{:<15}: {}'.format('Category',
                 CATEGORIES[json_output[i]['CategoryID']])
 
@@ -134,7 +163,7 @@ def pretty_print_simple_info(packages, baseurl=None, ood=True):
         print()
 
 def pretty_print_updpkgs(other_repos=[], baseurl=None, pkgs=[],
-        be_verbose=True):
+        be_verbose=True, color=False):
     '''
     Print a list of packages that need updating
 
@@ -144,20 +173,24 @@ def pretty_print_updpkgs(other_repos=[], baseurl=None, pkgs=[],
     baseurl (str) -- Where the AUR you are using is located
     ood (bool) -- Whether to show out of date items
     be_verbose (bool) -- Be verbose
+    color (bool) -- Whether to use color
     '''
+    _color = Color(color)
     if not isinstance(pkgs, list):
         raise TypeError('Must be a list')
     a = UpdatedPkgs(other_repos, pkgs=pkgs, baseurl=baseurl)
     upddict = a.get_upd_pkgs()
     for pkgs in sorted(upddict.keys()):
         if be_verbose:
-            print('{} {} => {}'.format(pkgs, upddict[pkgs]['oldver'],
-                upddict[pkgs]['newver']))
+            print('{3}::{7} {4}{0}{7} {5}{1}{7} => {6}{2}{7}'.format(
+                pkgs, upddict[pkgs]['oldver'], upddict[pkgs]['newver'],
+                _color.bold_blue, _color.bold, _color.bold_red,
+                _color.bold_green, _color.reset))
         else:
             print(pkgs)
 
 def download_pkgs(list_of_pkgs, dl_path, dl_verbose=True, baseurl=None,
-        dl_force=False, ood=True):
+        dl_force=False, ood=True, color=False):
     '''
     Download packages
 
@@ -166,7 +199,9 @@ def download_pkgs(list_of_pkgs, dl_path, dl_verbose=True, baseurl=None,
     dl_verbose (Bool) -- Whether to be verbose or not
     baseurl (str) -- Where the AUR you are using is located
     ood (bool) -- Whether to show out of date items
+    color (bool) -- Whether to use color
     '''
+    _color = Color(color)
     _a = GetPkgs(list_of_pkgs, baseurl=baseurl)
     _a.get_results()
     for i in range(len(_a.json_output)):
@@ -177,9 +212,13 @@ def download_pkgs(list_of_pkgs, dl_path, dl_verbose=True, baseurl=None,
             continue
         pkgver = _a.json_output[i]['Version']
         if path.exists('{}/{}'.format(dl_path, pkgname)) and not dl_force:
-            raise FileExists('{}/{} already exists. Use --force to overwrite'\
-                    .format(dl_path, pkgname))
+            raise FileExists('{}::{} {}/{} already exists. Use --force to overwrite'\
+                    .format(
+                        _color.bold_red, _color.reset,
+                        dl_path, pkgname))
         _a.get_stream(i)
         _a.get_tarfile(dl_path, force=dl_force)
         if dl_verbose:
-            print(':: {0} downloaded to {2}'.format(pkgname, pkgver, dl_path))
+            print('{2}::{4} {3}{0}{4} downloaded to {1}'.format(
+                pkgname, dl_path,
+                _color.bold_blue, _color.bold, _color.reset))
